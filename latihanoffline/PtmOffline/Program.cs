3dc1c010e9fdf620fb93b61f0c99ed0b990634a0
Savira Fatimah Zulfa <147517046+ChoiCha5@users.noreply.github.com>
2023-10-10 22:42:41 +0700

    Add files via upload

---
+++ latihanoffline/PtmOffline/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PtmOffline{
+    class Program{
+        static void Main(string[] args)
+        {
+            Laptop Asus = new Laptop();
+            Laptop Axio = new Laptop();
+            Processor Intel = new Processor();
+
+
+            Asus.hargaLaptop();
+            Axio.hargaLaptop();
+            Asus.PublicMethod();
+            Console.WriteLine(Intel.CORE);
+            Intel.hargaCORE();
+
+        }
+    }
+    class Laptop
+    {
+        private int harga = 1500000;
+        public void hargaLaptop()
+        {
+            Console.WriteLine(harga);
+        }
+        private void PrivateMethod()
+        {
+            Console.WriteLine("Private Method");
+        }
+        public void PublicMethod()
+        {
+            Console.WriteLine("Public Method");
+        }
+    }
+
+    class Processor
+    {
+        public int CORE = 100000;
+        public void hargaCORE()
+        {
+            Console.WriteLine("16 bit");
+        }
+
+        private void speed()
+        {
+            Console.WriteLine("8 bit");
+        }
+    }
+}
